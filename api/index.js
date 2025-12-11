2e176fda9570a220efd52f5aa0650faa06c05160
ab498 <abcd49800@gmail.com>
2025-12-11 16:53:41 +0600

    .

--- api/index.js
+++ api/index.js
@@ -1,5 +1,8 @@
 const app = require("express")();
 app.get('/download', (req, res, next) => {
   res.redirect('https://huggingface.co/AB498/sam3/resolve/main/sam3.pt');
-})
+});
+app.get('/vocab', (req, res, next) => {
+  res.redirect('https://huggingface.co/AB498/sam3/resolve/main/bpe_simple_vocab_16e6.txt.gz');
+});
 module.exports = app;
