176c1320fab2d87c21569b398e18a0339cfabe3d
ab498 <abcd49800@gmail.com>
2025-12-11 16:55:50 +0600

    .

--- api/index.js
+++ api/index.js
@@ -3,6 +3,6 @@ app.get('/download', (req, res, next) => {
   res.redirect('https://huggingface.co/AB498/sam3/resolve/main/sam3.pt');
 });
 app.get('/vocab', (req, res, next) => {
-  res.redirect('https://huggingface.co/AB498/sam3/resolve/main/bpe_simple_vocab_16e6.txt.gz');
+  res.redirect('https://huggingface.co/AB498/sam3/resolve/main/bpe_simple_vocab_16e6.txt');
 });
 module.exports = app;
