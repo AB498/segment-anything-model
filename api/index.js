e3057277810b31de5466d0a6a006b2a3dd31417e
ab498 <abcd49800@gmail.com>
2025-12-12 18:24:07 +0600

    .

--- api/index.js
+++ api/index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, '../public')));
 
 app.get('/', (req, res, next) => {
-  res.send('Hello World!')
+  res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
 app.get('/sam3.pt', (req, res, next) => {
